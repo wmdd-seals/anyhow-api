7a2bb29820d57114b269ac6527eb181d1aae033c
Uma Maheswara Rao <uvoleti00@mylangara.ca>
2024-11-22 10:49:20 -0800

    Tag Search

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -271,12 +271,22 @@ export const resolvers = {
         ): PromiseMaybe<Guides[]> {
             const guides = await context.prisma.guides.findMany({
                 where: {
-                    userId: args.userId,
-                    title: { search: args.search },
-                    body: { search: args.search },
-                    ...(args.published !== null && args.published !== undefined
-                        ? { published: args.published }
-                        : {})
+                    OR: [
+                        { userId: args.userId },
+                        { title: { search: args.search } },
+                        { body: { search: args.search } },
+                        {
+                            ...(args.search && {
+                                tags: { array_contains: [args.search ?? ''] }
+                            })
+                        },
+                        {
+                            ...(args.published !== null &&
+                            args.published !== undefined
+                                ? { published: args.published }
+                                : {})
+                        }
+                    ]
                 }
             })
 
